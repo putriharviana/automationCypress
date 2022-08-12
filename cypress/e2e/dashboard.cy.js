17805fe85356946ef96ebb6397259f8181e1147f
putriharviana <putriharviana@gmail.com>
2022-08-12 07:33:07 +0700

    initial commit

---
+++ cypress/e2e/dashboard.cy.js
@@ -0,0 +1,78 @@
+describe("Dashboard Page Test Cases", ()=> {
+    it("Visit login page", () => {
+        cy.visit("https://taufanfadhilah.github.io/react-gallery/");
+        cy.title().should("eq", "React Gallery"); //title should get string with name "React Gallery"
+        cy.contains("Hello Again!");
+    })
+
+    it("Do login with correct value", () => {
+        const email = cy.get("input[name='email']");
+        email.type("user@react.test");
+        
+        const password = cy.get("input[name='password']");
+        password.type("password");
+        
+        const button = cy.get("button").should("have.attr", "type", "submit");
+        button.click();
+        
+        cy.on("window:alert", (text) => {
+            expect(text).to.contains("welcome");
+        })
+
+        cy.url().should("eq", "https://taufanfadhilah.github.io/react-gallery/dashboard");
+    })
+
+    it("Found no post for the first time", ()=> {
+        cy.contains("Found 0 photos");
+    })
+
+    it("Contains image url and description input, and publish button", ()=> {
+        const imageUrl = cy.get("input[name='image']");
+        imageUrl.should("be.visible");
+        imageUrl.should("have.attr", "type", "url");
+        imageUrl.should("have.attr", "required", "required");
+        imageUrl.should("have.attr", "placeholder", "Image URL");
+
+        const description = cy.get("input[name='desc']");
+        description.should("be.visible");
+        description.should("have.attr", "type", "text");
+        description.should("have.attr", "required", "required");
+        description.should("have.attr", "placeholder", "What's on your mind?");
+
+        const button = cy.get("button").should("have.attr", "type", "submit");
+        button.should("be.visible");
+        button.contains("Publish!");
+        button.should("have.css", "background-color", "rgb(79, 70, 229)")
+    })
+
+    it("Upload some photos", ()=> {
+        const photos = [
+            {
+                imageValue:
+                    "https://images.unsplash.com/photo-1660092627183-8963c9c985c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+                descriptionValue: "Image 1 : Lorem Ipsum",
+            },
+            {
+                imageValue:
+                    "https://images.unsplash.com/photo-1660065079192-71368822b175?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+                descriptionValue: "Image 2 : Lorem Ipsum",
+            }
+        ];
+
+        photos.forEach(({imageValue, descriptionValue})=> {
+            const imageUrl = cy.get("input[name='image']");
+            imageUrl.type(imageValue);
+
+            const description = cy.get("input[name='desc']");
+            description.type(descriptionValue);
+            
+            const button = cy.get("button").should("have.attr", "type", "submit");
+            button.click();
+
+            cy.get("img").should("have.attr", "src", imageValue);
+            cy.contains(descriptionValue);
+        })
+
+        cy.contains(`Found ${photos.length} photos`); //String "Found x photos"
+    })
+})
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,64 @@
+describe ("Login page test cases", () => {
+    it("Visit login page", () => {
+        cy.visit("https://taufanfadhilah.github.io/react-gallery/");
+        cy.title().should("eq", "React Gallery"); //title should get string with name "React Gallery"
+        cy.contains("Hello Again!");
+    })
+
+    it("Contains Email and Password input and login button", () =>{
+        const email = cy.get("input[name='email']");
+        email.should("be.visible");
+        email.should("have.attr", "type", "email");
+        email.should("have.attr", "placeholder", "Email Address");
+
+        const password = cy.get("input[name='password']");
+        password.should("be.visible");
+        password.should("have.attr", "type", "password");
+        password.should("have.attr", "placeholder", "Password");
+
+        const button = cy.get("button").should("have.attr", "type", "submit");
+        button.contains("Login");
+        button.should("have.css", "background-color", "rgb(79, 70, 229)");
+        button.should("have.css", "color", "rgb(255, 255, 255)");
+    })
+
+    it("Do login with null value", () => {
+        const button = cy.get("button").should("have.attr", "type", "submit");
+        button.click();
+        cy.on("window:alert", (text) => {
+            expect(text).to.contains("login failed");
+        })
+    })
+
+    it("Do login with wrong value", () => {
+        const email = cy.get("input[name='email']");
+        email.type("testingemailerror@react.test");
+        
+        const password = cy.get("input[name='password']");
+        password.type("Password");
+        
+        const button = cy.get("button").should("have.attr", "type", "submit");
+        button.click();
+        
+        cy.on("window:alert", (text) => {
+            expect(text).to.contains("login failed");
+        })
+    })
+
+    it("Do login with correct value", () => {
+        const email = cy.get("input[name='email']");
+        email.type("user@react.test");
+        
+        const password = cy.get("input[name='password']");
+        password.type("password");
+        
+        const button = cy.get("button").should("have.attr", "type", "submit");
+        button.click();
+        
+        cy.on("window:alert", (text) => {
+            expect(text).to.contains("welcome");
+        })
+
+        cy.url().should("eq", "https://taufanfadhilah.github.io/react-gallery/dashboard");
+    })
+})
